c3f9ad94333a16bdb971a906c1c0b29ad0d48b54
Yiyi Wang <shd101wyy@sina.com>
2018-09-04 14:44:14 -0500

    .

--- divide-by-zero.c
+++ divide-by-zero.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int x = 0;
-    printf("%d\n", (4 / x));
+    int x = 9;
+    for (; x >= 0; x--) {
+        printf("%d\n", (4 / x));
+    }
     return 0;
 }
