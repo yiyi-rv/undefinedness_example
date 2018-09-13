f32cfc30f4338851c223b59cf2ebc6e6e214d36c
Yiyi Wang <shd101wyy@sina.com>
2018-09-13 15:57:45 -0500

    Test both circleci and travisci

--- rv.sh
+++ rv.sh
@@ -2,14 +2,14 @@
 echo "This is rv.sh"
 make
 ./1-unsequenced-side-effect
-./2-buffer-overflow
-./3-array-in-struct
-./4-buffer-underflow-external
-./5-buffer-overflow-environment
-./6-int-overflow
-./7-out-of-lifetime
-./8-int-overflow-tricky
-./9-memory-leak
+# ./2-buffer-overflow
+# ./3-array-in-struct
+# ./4-buffer-underflow-external
+# ./5-buffer-overflow-environment
+# ./6-int-overflow
+# ./7-out-of-lifetime
+# ./8-int-overflow-tricky
+# ./9-memory-leak
 rv-html-report my_errors.json -o report
 rv-upload-report `pwd`/report
 make clean  
